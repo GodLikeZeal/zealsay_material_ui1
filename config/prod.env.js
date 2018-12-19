007cffb6b96e06cdb9967db643cf0432c70744c1
zhanglei <zhanglei@i-counting.cn>
2018-12-19 19:57:21 +0800

    [feat]增加静态文件

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,6 +3,6 @@ module.exports = {
   NODE_ENV: '"production"',
   // ENV_CONFIG: '"prod"',
   // BASE_API: '"https://api-prod"'
-  BASE_API: '"http://47.105.51.137:8080"'
+  BASE_API: '"https://api.zealsay.com"'
 }
 
